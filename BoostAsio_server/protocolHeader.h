54e0d20375bd1ec35da1dd138d3eb1847357fcbb
wjdtlr0822 <a01055687052@gmail.com>
2024-08-30 01:54:45 +0900

    protocol 기능 추가중..

--- BoostAsio_server/protocolHeader.h
+++ BoostAsio_server/protocolHeader.h
@@ -4,14 +4,38 @@
 #define DELETE_ROOM 0x0002
 #define SEND_MESSAGE 0x0003
 
-
 /*
-	protocol 구조
-	Header {
+* 모든 프로코톨 header
+* Header {
 		Length : uint16;
 		code : uint16;
 	}
-	Message{
-		message : string;
+
+----------------------------------------------------------------------
+* CREATE_ROOM 
+(client -> server)
+	body{
+		room name : char[]
 	}
+(server -> client)
+	body{
+		result : bool;
+	}
+----------------------------------------------------------------------
+	DELETE_ROOM
+(client -> server)
+	body{
+		room name : char[]
+	}
+(server -> client)
+	body{
+		result : bool;
+	}
+----------------------------------------------------------------------
+	SEND_MESSAGE
+(server <-> client)
+	body{
+		message : char[];
+	}
+----------------------------------------------------------------------
 */
